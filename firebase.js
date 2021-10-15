b807a063228b401ea05a65a8ea8af51096e62d15
aditya77kushwaha <adityakushwaha2912@gmail.com>
2021-10-15 21:58:08 +0530

    configs Firebase

--- firebase.js
+++ firebase.js
@@ -1,10 +1,25 @@
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import * as firebase from "firebase";
+import "firebase/auth";
+import "firebase/database";
+
 const firebaseConfig = {
-    apiKey: "AIzaSyDJwNQMcI9gW9UyhWTedAqciTyzWnkIXpk",
-    authDomain: "signal-clone-6a428.firebaseapp.com",
-    projectId: "signal-clone-6a428",
-    storageBucket: "signal-clone-6a428.appspot.com",
-    messagingSenderId: "875447939887",
-    appId: "1:875447939887:web:9c3c1c49ffac95ecc827da",
-    measurementId: "G-M0SLLQ21VM"
+  apiKey: "AIzaSyDJwNQMcI9gW9UyhWTedAqciTyzWnkIXpk",
+  authDomain: "signal-clone-6a428.firebaseapp.com",
+  projectId: "signal-clone-6a428",
+  storageBucket: "signal-clone-6a428.appspot.com",
+  messagingSenderId: "875447939887",
+  appId: "1:875447939887:web:9c3c1c49ffac95ecc827da",
+  measurementId: "G-M0SLLQ21VM",
 };
+
+let app;
+if (firebase.apps.length === 0) {
+  app = firebase.initializeApp(firebaseConfig);
+} else {
+  app = firebase.app();
+}
+
+const db = app.firestore();
+const auth = firebase.auth();
+
+export { db, auth };
